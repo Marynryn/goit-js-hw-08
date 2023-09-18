1325fa9b7834e32ba15be04fa3264a9a383e2d32
Marynryn <murmar95@gmail.com>
2023-09-18 08:33:27 +0300

    summary

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,42 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const list = document.querySelector(".gallery");
+
+list.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `
+      <li class="gallery__item">
+      <a class="gallery__link" href=${original}>
+        <img
+          class="gallery__image"
+          src=${preview}
+          data-source=${original}
+          alt=${description}
+        />
+      </a>
+    </li>
+  `
+    )
+    .join("");
+   
+}
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+  captionsData: `alt`,
+  captionDelay: 250,
+})
+
+ 
+  
+  
+ 
+  
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
 
+import throttle from "lodash.throttle";
+ const emailInput = document.querySelector(".feedback-form email");
+ const textArea = document.getElementsByTagName(".feedback-form textarea");
+ const list = document.querySelector(".feedback-form");
+ const KeyIn = "feedback-form-state";
+list.addEventListener("input", throttle(inputInfo, 500));
+
+const formInfo = {};
+function inputInfo(event){
+    formInfo[event.target.name] = event.target.value;
+    localStorage.setItem(KeyIn,JSON.stringify(formInfo));
+    
+}
+const info = JSON.parse(localStorage.getItem(KeyIn));
+list.addEventListener("sumbit", submitEvent);
+function submitEvent(event){
+   if(info < 2){
+    alert("Потрібно заповнити усі поля")
+   } else{
+
+    console.log(info.email, info.message);
+    event.preventDefault()
+ 
+    localStorage.removeItem(KeyIn);}
+}
+// list.on("set", setForm)
+// function setForm( info){
+//     localStorage.getItem(KeyIn);
+//     emailInput.value = info.email;
+//     textArea.value = info.massage;
+// }
